acc90af54bba8c1467eed9df8ee152ff89ebd54e
Thysmall <jordan@dbimages.com>
2026-01-21 13:03:42 -0500

    Previous changes from demonstration

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,22 @@
-group: str = "Universe"
-print(f"Hello {group}")
-print(f"Hello {group}")
-print(f"Hello {group}")
+# title: str = "Bob's Gas Station Receipt"
+# print(f"Hello {group:<20}!")
+# print(f"Hello {group:^20}!")
+# print(f"Hello {group:>20}!")
+# print(f'{title:_^50}')
+# slope intercept form bad practice
+# m = 5
+# x = 2
+# b = 6
+# y = (m * x) + b
+
+#naming conventions
+#  great practice
+# age_to_enter: int = 21
+#  too long
+# the_value_that_needs_to_be_implemented_before_the_game_starts: int = 25
+#  upgrade
+# initial_start_value: int = 25
+
+cost: float = 15.6799
+print(f"${cost:.2f}")
+
